8300112b395b7bd46166900cf81bc267a6d48c2a
Joyce Chen <joyce.chen721@gmail.com>
2023-10-22 18:12:28 -0700

    add error messages

--- lab1/pipe.c
+++ lab1/pipe.c
@@ -9,6 +9,7 @@ int main(int argc, char *argv[])
 	//handle no arguments
 	if (argc == 1) {
 		errno = EINVAL;
+		fprintf(stderr, "must include at least 1 argument\n");
 		exit(errno);
 	}
 
@@ -20,6 +21,7 @@ int main(int argc, char *argv[])
 		//create pipe
 		int fds[2];
 		if (pipe(fds) == -1){
+			fprintf(stderr, "error creating pipe\n");
 			exit(errno);
 		}
 		//fork the program
@@ -29,14 +31,17 @@ int main(int argc, char *argv[])
         if (child_pid == 0) {
 			//close read-end
 			if (close(fds[0]) == -1){
+				fprintf(stderr, "error closing pipe read end\n");
 				exit(errno);
 			}
 			//set stdout to write-end of pipe
 			if(dup2(fds[1], 1) < 0){
+				fprintf(stderr, "error redirecting stdout to pipe write end\n");
 				exit(errno);
 			}
 			//handle invalid programs
 			if (execlp(argv[proc_num], argv[proc_num], NULL) == -1) {
+				fprintf(stderr, "invalid arguments\n");
 				exit(errno);
    			}
         }
@@ -47,26 +52,31 @@ int main(int argc, char *argv[])
 			//check whether child exited with error
 			int exit_status = WEXITSTATUS(status);
 			if(exit_status != 0) {
+				fprintf(stderr, "error running child process\n");
 				exit(exit_status);
 			}
 			//set stdin to read-end of pipe
-			if(dup2(fds[0], 0)){
+			if(dup2(fds[0], 0) < 0){
+				fprintf(stderr, "error setting stdin\n");
 				exit(errno);
 			}
 			//close write-end
 			if(close(fds[1]) == -1){
+				fprintf(stderr, "error closing write end of pipe\n");
 				exit(errno);
 			}
         }
 
 		//failed fork()
 		else {
+			fprintf(stderr, "fork failed\n");
 			exit(errno);
 		}
     }
 
 	//execute the last instruction (stdout is default)
 	if (execlp(argv[proc_num], argv[proc_num], NULL) == -1) {
+		fprintf(stderr, "error executing last process\n");
 		exit(errno);
 	}
 
